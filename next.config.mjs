d87256c6e6c38c535e64cc5e6a16147c1c2c6cc2
IDgaram <lorys.lellouche@gmail.com>
2024-10-15 10:08:34 +0200

    package json starts on p3000 h 0.0.0.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
